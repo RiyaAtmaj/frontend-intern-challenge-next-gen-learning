1d3befd620c075369f72db6ef5434cc5c2bbe5a7
Riya Atmaj <riyaatmaj914@gmail.com>
2026-05-25 10:18:03 +0000

    Revert "Fix hydration error by suppressing warning on body element"
    This reverts commit 548b74405963b61c4d5326b603616a76502c930a.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
